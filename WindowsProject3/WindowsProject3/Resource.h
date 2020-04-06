482b62e4af925e01a2c13989685816caf9bc6b75
Subhash.S <52845663+SubhashStark@users.noreply.github.com>
2020-04-06 20:45:21 +0530

    Changes in registry key application

---
+++ WindowsProject3/WindowsProject3/Resource.h
@@ -0,0 +1,35 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by WindowsProject3.rc
+//
+#define IDC_MYICON                      2
+#define IDD_WINDOWSPROJECT3_DIALOG      102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_WINDOWSPROJECT3             107
+#define IDI_SMALL                       108
+#define IDC_WINDOWSPROJECT3             109
+#define IDR_MAINFRAME                   128
+#define IDR_MENU1                       129
+#define IDD_DIALOG1                     132
+#define ID_KEY_DIALOG                   132
+#define IDCANCEL                        1002
+#define CREATE_BUTTON                   1003
+#define DELETE_BUTTON                   1004
+#define ID_FILE_KEY                     32771
+#define ID_FILE_EXIT                    32774
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_COMMAND_VALUE         32775
+#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
