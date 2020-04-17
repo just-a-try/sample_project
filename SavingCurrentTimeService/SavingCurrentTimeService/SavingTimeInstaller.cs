2417561923760e5943ffd24573086232cf2bada7
Subhash.S <52845663+SubhashStark@users.noreply.github.com>
2020-04-17 23:22:24 +0530

    Initial commit for saving currrent time service programs

---
+++ SavingCurrentTimeService/SavingCurrentTimeService/SavingTimeInstaller.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SavingCurrentTimeService
+{
+    class SavingTimeInstaller
+    {
+        [System.ComponentModel.RunInstaller(true)]
+        public class ProjectInstaller : System.Configuration.Install.Installer
+        {
+            private System.ServiceProcess.ServiceInstaller serviceInstaller;
+            private System.ServiceProcess.ServiceProcessInstaller
+                    serviceProcessInstaller;
+
+            public ProjectInstaller()
+            {
+                InitializeComponent();
+            }
+
+            private void InitializeComponent()
+            {
+                this.serviceInstaller = new System.ServiceProcess.ServiceInstaller();
+                this.serviceProcessInstaller =
+                new System.ServiceProcess.ServiceProcessInstaller();
+             
+                this.serviceInstaller.Description = "My Windows Service description";
+                this.serviceInstaller.DisplayName = "My WinService";
+                this.serviceInstaller.ServiceName = "WinService";
+               
+                this.serviceProcessInstaller.Account =
+                System.ServiceProcess.ServiceAccount.LocalService;
+                this.serviceProcessInstaller.Password = null;
+                this.serviceProcessInstaller.Username = null;
+             
+                this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+                this.serviceProcessInstaller,
+                this.serviceInstaller});
+            }
+        }
+    }
+}
---
+++ SavingCurrentTimeService/SavingCurrentTimeService/SavingTimeService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SavingCurrentTimeService
+{
+    class SavingTimeService : System.ServiceProcess.ServiceBase
+    {
+        static void Main()
+        {
+            System.ServiceProcess.ServiceBase[] ServicesToRun;
+            ServicesToRun =
+              new System.ServiceProcess.ServiceBase[] { new WinService() };
+            System.ServiceProcess.ServiceBase.Run(ServicesToRun);
+        }
+        private void InitializeComponent()
+        {
+            this.ServiceName = "WinService";
+        }
+    
+        protected override void OnStart(string[] args)
+        {
+            FILE* fp = NULL;
+            SYSTEMTIME time_info;
+            TIME t;
+            DWORD sleep_time = 65000;
+
+            while (1)
+            {
+                Sleep(sleep_time);
+                GetLocalTime(&time_info);
+                t.hour = time_info.wHour;
+                t.minute = time_info.wMinute;
+
+                if (!(fp = fopen("time.txt", "wb")))
+                {
+                    cout << "file not opened properly" << endl;
+                    break;
+                }
+
+                if (fwrite(&t, 4, NMEB, fp) == EOF)
+                {
+                    cout << "fwrite is not excecuted successfully" << endl;
+                    break;
+                }
+
+                if (fp)
+                {
+                    if (fclose(fp) == EOF)
+                    {
+                        cout << "The file is not closed successfully" << endl;
+                    }
+                }
+            }
+
+        }
+    }
+}
