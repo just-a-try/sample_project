66f3345387b199fd231f7f2351f951fa160a00f9
Subhash.S <52845663+SubhashStark@users.noreply.github.com>
2020-04-20 16:52:52 +0530

    Commit for Dll project and Service program

---
+++ SavingCurrentTimeService/SavingCurrentTimeService/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace SavingCurrentTimeService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitCode = HostFactory.Run(x =>
+            {
+                x.Service<SavingCurrentTime>(s =>
+                {
+                    s.ConstructUsing(savingCurrentTime => new SavingCurrentTime());
+                    s.WhenStarted(savingCurrentTime => savingCurrentTime.Start());
+                    s.WhenStopped(savingCurrentTime => savingCurrentTime.Stop());
+                });
+
+                x.RunAsLocalSystem();
+
+                x.SetServiceName("SavingCurrentTime");
+                x.SetDisplayName("Saving Current Time");
+                x.SetDescription("This is a service program which writes the current system time in a text file.");
+            });
+
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+        }
+    }
+}
---
+++ SavingCurrentTimeService/SavingCurrentTimeService/SavingCurrentTime.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace SavingCurrentTimeService
+{
+    public class SavingCurrentTime
+    {
+        private readonly Timer timer;
+
+        public SavingCurrentTime()
+        {
+            timer = new Timer(65000) { AutoReset = true };
+            timer.Elapsed += TimerElapsed;
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            string[] current_time = new string[] { DateTime.Now.ToString("h:mm:ss tt") };
+            File.AppendAllLines(@"C:\SaveCurrentTime.txt", current_time);
+        }
+
+        public void Start()
+        {
+            timer.Start();
+        }
+
+        public void Stop()
+        {
+            timer.Stop();
+        }
+    }
+}
