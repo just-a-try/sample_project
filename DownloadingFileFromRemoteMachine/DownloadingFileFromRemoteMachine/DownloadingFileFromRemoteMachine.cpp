40625319047e62b884b28dad2af7fe35e3b0ae5b
Subhash.S <52845663+SubhashStark@users.noreply.github.com>
2020-05-11 10:18:16 +0530

    commit for remaining windows applications

---
+++ DownloadingFileFromRemoteMachine/DownloadingFileFromRemoteMachine/DownloadingFileFromRemoteMachine.cpp
@@ -0,0 +1,263 @@
+// DownloadingFileFromRemoteMachine.cpp : Defines the entry point for the application.
+//
+
+#include "framework.h"
+#include "DownloadingFileFromRemoteMachine.h"
+#include "Resource.h"
+#include <commctrl.h>
+#include <commdlg.h>
+#include "shlwapi.h"
+
+#define MAX_LOADSTRING 100
+
+// Global Variables:
+HINSTANCE hInst;                                // current instance
+WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+
+// Forward declarations of functions included in this code module:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    Download(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Place code here.
+
+    // Initialize global strings
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_DOWNLOADINGFILEFROMREMOTEMACHINE, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Perform application initialization:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_DOWNLOADINGFILEFROMREMOTEMACHINE));
+
+    MSG msg;
+
+    // Main message loop:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_DOWNLOADINGFILEFROMREMOTEMACHINE));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_DOWNLOADINGFILEFROMREMOTEMACHINE);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+
+//
+//   FUNCTION: InitInstance(HINSTANCE, int)
+//
+//   PURPOSE: Saves instance handle and creates main window
+//
+//   COMMENTS:
+//
+//        In this function, we save the instance handle in a global variable and
+//        create and display the main program window.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Store instance handle in our global variable
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   DialogBox(hInst, MAKEINTRESOURCE(ID_DOWNLOAD_DIALOG), hWnd, Download);
+   DestroyWindow(hWnd);
+  /* ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);*/
+
+   return TRUE;
+}
+
+//
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE: Processes messages for the main window.
+//
+//  WM_COMMAND  - process the application menu
+//  WM_PAINT    - Paint the main window
+//  WM_DESTROY  - post a quit message and return
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // Parse the menu selections:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // TODO: Add any drawing code that uses hdc here...
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
+
+// Message handler for Download dialog box.
+INT_PTR CALLBACK Download(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == ID_DOWNLOAD_BUTTON)
+		{
+			WCHAR curfile[MAX_PATH];
+			curfile[0] = '\0';
+			OPENFILENAME ofn;
+			ZeroMemory(&ofn, sizeof(OPENFILENAME));
+			ofn.lStructSize = sizeof(OPENFILENAME);
+			ofn.hwndOwner = hDlg;
+			ofn.lpstrFilter = L"All File(*.*)\0*.*\0";
+			ofn.lpstrFile = curfile;
+			ofn.nMaxFile = MAX_PATH;
+			ofn.Flags = OFN_EXPLORER | OFN_FILEMUSTEXIST | OFN_HIDEREADONLY;
+			ofn.lpstrDefExt = L"";
+			if (!GetOpenFileName(&ofn))
+				return FALSE;
+			MessageBox(hDlg, curfile, L"Path", MB_OK);
+
+			HANDLE hFile = CreateFile(curfile, GENERIC_READ, FILE_SHARE_READ,
+				(LPSECURITY_ATTRIBUTES)NULL, OPEN_EXISTING,
+				FILE_ATTRIBUTE_NORMAL, (HANDLE)NULL);
+
+			if (hFile)
+			{
+				MessageBox(hDlg, L"File handle present", L"status", MB_OK);
+			}
+
+			DWORD cb = GetFileSize(hFile, (LPDWORD)NULL);
+			HWND hwndpb = GetDlgItem(hDlg, ID_DOWNLOAD_PROGRESS);
+
+			SendMessage(hwndpb, PBM_SETRANGE, 0, MAKELPARAM(0, cb / 2048));
+
+			SendMessage(hwndpb, PBM_SETSTEP, (WPARAM)1, 0);
+
+			SendMessage(GetDlgItem(hDlg, ID_DOWNLOAD_PROGRESS), PBM_SETSTEP, 50, 0);
+			/*if (PathFileExists(L"C:\\Users\\ELCOT\\Downloads\\movie.mkv"))
+			{
+				MessageBox(hDlg, L"File present", L"status", MB_OK);
+			}
+			else
+			{
+				MessageBox(hDlg, L"file not present", L"status", MB_OK);
+			}*/
+
+			if (!CopyFile(curfile, L"C:\\Users\\ELCOT\\Downloads\\movie.mkv", TRUE))
+			{
+				MessageBox(hDlg, L"Failed", L"status", MB_OK);
+			}
+
+			//SendMessage(GetDlgItem(hDlg, ID_DOWNLOAD_PROGRESS), PBM_SETSTEP, 10, 0);
+			return (INT_PTR)TRUE;
+		}
+		else if(LOWORD(wParam) == ID_CANCEL_BUTTON)
+		{
+			return (INT_PTR)TRUE;
+		}
+		else if(LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
---
+++ SendingTextToOtherApplication/SendingTextToOtherApplication/SendingTextToOtherApplication.cpp
@@ -0,0 +1,263 @@
+// SendingTextToOtherApplication.cpp : Defines the entry point for the application.
+//
+
+#include "framework.h"
+#include "SendingTextToOtherApplication.h"
+#include "Resource.h"
+
+#define MAX_LOADSTRING 100
+
+// Global Variables:
+HINSTANCE hInst;                                // current instance
+WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+
+// Forward declarations of functions included in this code module:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    Send(HWND, UINT, WPARAM, LPARAM);
+BOOL SendData(HWND hDlg);
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Place code here.
+
+    // Initialize global strings
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_SENDINGTEXTTOOTHERAPPLICATION, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Perform application initialization:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_SENDINGTEXTTOOTHERAPPLICATION));
+
+    MSG msg;
+
+    // Main message loop:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_SENDINGTEXTTOOTHERAPPLICATION));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_SENDINGTEXTTOOTHERAPPLICATION);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   FUNCTION: InitInstance(HINSTANCE, int)
+//
+//   PURPOSE: Saves instance handle and creates main window
+//
+//   COMMENTS:
+//
+//        In this function, we save the instance handle in a global variable and
+//        create and display the main program window.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Store instance handle in our global variable
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   DialogBox(hInst, MAKEINTRESOURCE(ID_SEND_DIALOG), hWnd, Send);
+   DestroyWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE: Processes messages for the main window.
+//
+//  WM_COMMAND  - process the application menu
+//  WM_PAINT    - Paint the main window
+//  WM_DESTROY  - post a quit message and return
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // Parse the menu selections:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // TODO: Add any drawing code that uses hdc here...
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
+
+// Message handler for Send box.
+INT_PTR CALLBACK Send(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == ID_SEND_BUTTON)
+		{
+			if (!SendData(hDlg))
+			{
+				OutputDebugString(L"SendData function failed\n");
+			}
+		}
+		else if(LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
+
+/* @author   : Subhash
+   @function : to send the user given text to the other application
+   @param    : Handle of the dislog box from where it is sent
+   @return   : True if function succeed or False if it fails
+*/
+BOOL SendData(HWND hwnd)
+{
+	HWND edit = GetDlgItem(hwnd, ID_TEXTBOX);
+
+	if (!edit)
+	{
+		OutputDebugString(L"GetDlgItem api failed\n");
+		return FALSE;
+	}
+
+	DWORD dwTextLength;
+	dwTextLength = GetWindowTextLength(edit);
+
+	if (dwTextLength < 0)
+	{
+		OutputDebugString(L"GetWindowTextLength api failed\n");
+		return FALSE;
+	}
+
+	COPYDATASTRUCT data_to_send = {0};
+	LPSTR pszText;
+	DWORD dwBufferSize = dwTextLength + 1;
+	pszText = (char*)GlobalAlloc(GPTR, dwBufferSize);
+
+	if (!GetWindowTextLength)
+	{
+		OutputDebugString(L"Memory not allocated for pszText\n");
+		return FALSE;
+	}
+
+	GetWindowTextA(edit, pszText, dwBufferSize);
+	data_to_send.dwData = 1;
+	data_to_send.cbData = dwBufferSize;
+	data_to_send.lpData = pszText;
+
+	HWND hnd = FindWindow(NULL, L"ReceiveTextApplication");
+	if (hnd)
+	{
+		//MessageBox(hDlg, L"Receiving app present", L"App status", MB_OK);
+		SendMessage(hnd, WM_COPYDATA, 0, (LPARAM)(LPVOID)&data_to_send);
+	}
+	else
+	{
+		MessageBox(NULL, L"Receiving app not present", L"App status", MB_OK);
+	}
+
+	return TRUE;
+}
